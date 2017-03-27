866c3cc02fd1d0fdb1e31d384c1bc5da4dc43448
John Scarbrough <jpscarbrough@fedex.com>
2017-03-27 12:34:47 -0400

    Initial commit of project maven-spring-demo-data
    Change-Id: I9ec6157e75aea9a078658fb953bed7651d51e13b

---
+++ src/main/java/com/fedex/jps/MavenSpringDemoDataApplication.java
@@ -0,0 +1,12 @@
+package com.fedex.jps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenSpringDemoDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenSpringDemoDataApplication.class, args);
+	}
+}
