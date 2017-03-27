1f6a8cf113d73f6c1affd365b55095783317fd85
John Scarbrough <jpscarbrough@fedex.com>
2017-03-27 12:37:07 -0400

    moving more of the application (3)

---
+++ src/test/java/com/fedex/jps/MavenSpringDemoDataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fedex.jps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MavenSpringDemoDataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
